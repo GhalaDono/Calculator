5bbf4ea39fd20ed12c42e8abb48c535887e7175b
Ghala <aghala937@gmail.com>
2024-06-24 19:06:06 +0300

    grid

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,28 @@
 <svelte:head>
    <title>الة حاسبة</title> 
 </svelte:head>
-    <div class="bg-[#ffffff] h-[30rem] w-[20rem] rounded-3xl flex justify-center items-center">
-        <div class="bg-black h-[4rem] w-[4rem] rounded-full flex justify-center">
-
-        </div>
+    <div class="bg-[#fdfefe]  
+    rounded-3xl grid grid-cols-4 gap-1 "> 
+        <button>%</button>
+        <button>√</button>
+        <button>CE</button>
+        <button>C</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>-</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>÷</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>×</button>
+        <button>.</button>
+        <button>0</button>
+        <button>=</button>
+        <button>+</button>
+       
     </div>
 
