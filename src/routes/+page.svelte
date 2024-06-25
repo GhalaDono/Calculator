eeaab2fc709cf11581beda8360f938c75fd1012a
Ghala <aghala937@gmail.com>
2024-06-25 14:02:56 +0300

    shdow1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,35 @@
 <svelte:head>
-   <title>الة حاسبة</title> 
+   <title>Calculator</title> 
 </svelte:head>
-    <div class="bg-[#fdfefe]  
-    rounded-3xl grid grid-cols-4 gap-1 "> 
-        <button>%</button>
-        <button>√</button>
-        <button>CE</button>
-        <button>C</button>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button>-</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>÷</button>
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>×</button>
-        <button>.</button>
-        <button>0</button>
-        <button>=</button>
-        <button>+</button>
+
+    <div class="bg-[#fdfefe] rounded-3xl 
+    grid grid-cols-4 gap-1 p-5 font-bold shadow-xl">
+
+    <div class="bg-[#2386fe] rounded-full col-span-4 h-11
+     flex items-center px-2 mb-2 text-white justify-end text-right ">
+12345
+    </div>
+
+        <button class="bg-[#f3f6fd] text-[#5e657e]">%</button>
+        <button class="bg-[#f3f6fd] text-[#5e657e]">√</button> 
+        <button class="bg-[#f3f6fd] text-[#5e657e]">CE</button>
+        <button class="bg-[#1f2b55] text-white">C</button>
+        <button class="text-[#5e657e]">7</button>
+        <button class="text-[#5e657e]">8</button>
+        <button class="text-[#5e657e]">9</button>
+        <button class="bg-[#fb4557] text-white shadow-[#fb4557]">-</button>
+        <button class="text-[#5e657e]">4</button>
+        <button class="text-[#5e657e]">5</button>
+        <button class="text-[#5e657e]">6</button>
+        <button class="bg-[#2386fe] text-white shadow-[#2386fe]">÷</button>
+        <button class="text-[#5e657e]">1</button>
+        <button class="text-[#5e657e]">2</button>
+        <button class="text-[#5e657e]">3</button>
+        <button class="bg-[#fac901]  text-white shadow-[#fac901]">×</button>
+        <button class="text-[#5e657e]">.</button>
+        <button class="text-[#5e657e]">0</button>
+        <button class="bg-[#f3f6fd] text-[#5e657e]">=</button>
+        <button class="bg-[#5de16f] text-white shadow-[#5de16f]">+</button>
        
     </div>
 
