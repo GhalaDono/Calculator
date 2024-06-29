67518621c8839d13f40de5ba1c8119f8fda33f2f
Ghala <aghala937@gmail.com>
2024-06-29 16:53:01 +0300

    done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,7 @@
     import Minus from "$lib/icons/Minus.svelte";
     import Multiply from "$lib/icons/Multiply.svelte";
     import Backsp from "$lib/icons/Backsp.svelte";
+    import { onMount } from "svelte";
 
     let equation: string = "";
 
@@ -31,29 +32,49 @@
     }
 
 
-
-
-
-
-
     function solve() {
         try {
           let answer= eval(equation);
           if(typeof answer === 'undefined') throw SyntaxError;
           equation=answer;
         } catch (error) {
-            let output = document.getElementById("output") as HTMLElement;
+            let output = document.getElementById("output");
             output?.classList.add("bg-red-500");
             setTimeout(() => {
                 output?.classList.remove("bg-red-500");
             }, 500);
         }
     }
+
+    function onKeyDown(e : KeyboardEvent){
+      
+      let button= document.getElementById(e.key);
+      button?.click();
+      button?.focus();
+      setTimeout(() => {
+        //@ts-ignore
+                document.activeElement?.blur();
+            }, 100);
+
+    }
+
+    onMount(()=>{ let allButtons =document.getElementsByTagName('button');
+    console.log(allButtons);
+    for(let i=0; i<allButtons.length;i++){
+        allButtons[i].addEventListener('click',()=>{
+
+            new Audio('/click1.wav').play();
+        });
+    }
+   }
+    );
 </script>
 
 <svelte:head>
     <title>Calculator</title>
 </svelte:head>
+
+<svelte:window on:keydown|preventDefault={onKeyDown} />
 
 <div
     class="bg-[#fdfefe] rounded-3xl max-w-[16rem]
@@ -67,56 +88,57 @@
         {equation}
     </div>
 
-    <button on:click={clear} class="bg-[#f3f6fd]">AC</button>
+    <button id="Delete" on:click={clear} class="bg-[#f3f6fd]">AC</button>
 
-    <button on:click={BackSpace} class="bg-[#f3f6fd]">
+    <button id="B2=3=ackspace" on:click={BackSpace} class="bg-[#f3f6fd]">
         <Backsp />
     </button>
 
-    <button on:click={() => addToEquation(" / 100")} class="bg-[#f3f6fd]"
+    <button id="%" on:click={() => addToEquation(" / 100")} class="bg-[#f3f6fd]"
         >%</button
     >
 
-    <button
+    <button id="+"
         on:click={() => addToEquation(" + ")}
         class="bg-[#5de16f] text-white shadow-[0_1px_6px_0_#b4fbbe]"
     >
         <Addition />
     </button>
 
-    <button on:click={() => addToEquation("7")}>7</button>
-    <button on:click={() => addToEquation("8")}>8</button>
-    <button on:click={() => addToEquation("9")}>9</button>
-    <button
+    <button id="7" on:click={() => addToEquation("7")}>7</button>
+    <button id="8" on:click={() => addToEquation("8")}>8</button>
+    <button id="9" on:click={() => addToEquation("9")}>9</button>
+    <button id="-"
         on:click={() => addToEquation(" - ")}
         class="bg-[#fb4557] text-white shadow-[0px_01px_6px_0px_#f97d8a]"
     >
         <Minus />
     </button>
 
-    <button on:click={() => addToEquation("4")}>4</button>
-    <button on:click={() => addToEquation("5")}>5</button>
-    <button on:click={() => addToEquation("6")}>6</button>
-    <button
+    <button id="4" on:click={() => addToEquation("4")}>4</button>
+    <button id="5" on:click={() => addToEquation("5")}>5</button>
+    <button id="6" on:click={() => addToEquation("6")}>6</button>
+    <button id="/"
         on:click={() => addToEquation(" / ")}
         class="bg-[#2386fe] text-white shadow-[0_1px_6px_0_#a5cbf9]"
     >
         <Divide />
     </button>
 
-    <button on:click={() => addToEquation("1")}>1</button>
-    <button on:click={() => addToEquation("2")}>2</button>
-    <button on:click={() => addToEquation("3")}>3</button>
-    <button
+    <button id="1" on:click={() => addToEquation("1")}>1</button>
+    <button id="2" on:click={() => addToEquation("2")}>2</button>
+    <button id="3" on:click={() => addToEquation("3")}>3</button>
+    <button id="*"
         on:click={() => addToEquation(" * ")}
         class="bg-[#fac901] text-white shadow-[0_2px_6px_0px_#f6df85]"
     >
         <Multiply />
     </button>
 
-    <button on:click={() => addToEquation("0")}>0</button>
-    <button on:click={() => addToEquation(".")}>.</button>
-    <button on:click={() => solve()} class="bg-[#f3f6fd] col-span-2">
+    <button id="0" on:click={() => addToEquation("0")}>0</button>
+    <button id="." on:click={() => addToEquation(".")}>.</button>
+    <button id="=" on:click={() => solve()} class="bg-[#f3f6fd] col-span-2">
         <Equals />
     </button>
 </div>
+ 
